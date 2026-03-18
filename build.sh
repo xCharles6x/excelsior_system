638d8282f800198508396e3a478d85327dab5b2d
charles <Charlestiongco132@gmail.com>
2026-03-18 09:30:58 +0800

    Fix settings module path in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+
+DJANGO_SETTINGS_MODULE=excelsior_system.settings python manage.py collectstatic --no-input
+DJANGO_SETTINGS_MODULE=excelsior_system.settings python manage.py migrate
